21fb47ce71bb0004cbbefee0fd5c0967cd1d5bc0
hrasel <59330627+hrasel@users.noreply.github.com>
2023-06-20 17:23:03 +0600

    Add files via upload

---
+++ Learning/app.js
@@ -0,0 +1,477 @@
+const lessonsWebDesign = [
+    // {
+    //     name: 'overview',
+    //     day: 1,
+    //     badge: 'complete',
+    //     time: '5m',
+    //     paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+    //     list: [
+    //         {
+    //             title: 'Color',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'Typrography',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'Fundamentals of Design',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'Photoshop',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'Canva',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'Figma',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'Web Layouts',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+    //         {
+    //             title: 'UI/UX',
+    //             time: '1d',
+    //             link: '#',
+    //             linkText: function() {
+    //                 return 'Jump to ' + this.title;
+    //             }
+    //         },
+
+    //     ]
+    // },
+    {
+        name: 'Color',
+        day: 1,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: 'Color Theory for Designers, Part 1',
+                time: '1h',
+                link: 'https://www.smashingmagazine.com/2010/01/color-theory-for-designers-part-1-the-meaning-of-color/',
+                linkText: 'The Meaning of Color'
+            },
+            {
+                title: 'Color Theory for Designers, Part 2',
+                time: '1h',
+                link: 'https://www.smashingmagazine.com/2010/02/color-theory-for-designers-part-2-understanding-concepts-and-terminology/',
+                linkText: 'Understanding Concepts And Color Terminology'
+            },
+            {
+                title: 'Color Theory for Designers, Part 3',
+                time: '1h',
+                link: 'https://www.smashingmagazine.com/2010/02/color-theory-for-designer-part-3-creating-your-own-color-palettes/',
+                linkText: 'How To Create Your Own Color Schemes'
+            },
+            {
+                title: 'Advanced Color Theory',
+                time: '1h',
+                link: 'https://design.tutsplus.com/articles/advanced-color-theory-what-is-color-management--cms-26307',
+                linkText: 'Advanced Color Theory: What Is Color Management?'
+            },
+            {
+                title: "A Simple Web Developer’s Color Guide",
+                time: '1h',
+                link: 'https://www.smashingmagazine.com/2016/04/web-developer-guide-color/',
+                linkText: "A Simple Web Developer’s Color Guide"
+            },
+            {
+                title: 'Color Contrast',
+                time: '1h',
+                link: 'https://www.smashingmagazine.com/2014/10/color-contrast-tips-and-tools-for-accessibility/',
+                linkText: 'Color Contrast And Why You Should Rethink It'
+            },
+            {
+                title: 'Color Theory 101',
+                time: '1h',
+                link: 'https://blog.hubspot.com/marketing/color-theory-design',
+                linkText: 'A Complete Guide to Color Wheels & Color Schemes'
+            },
+            {
+                title: 'Choosing colors for web design',
+                time: '1h',
+                link: 'https://dribbble.com/stories/2018/12/19/choosing-colors-for-web-design-a-practical-ui-color-application-guide',
+                linkText: 'A practical UI color application guide'
+            }
+
+        ]
+    },
+    {
+        name: 'Typrography',
+        day: 2,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: 'What Is Typography',
+                time: '1h',
+                link: 'https://careerfoundry.com/en/blog/ui-design/beginners-guide-to-typography/',
+                linkText: 'What Is Typography, and Why Is It Important? A Beginner’s Guide'
+            },
+            {
+                title: 'Understanding typography',
+                time: '1h',
+                link: 'https://m2.material.io/design/typography/understanding-typography.html#type-properties',
+                linkText: 'Understanding typography'
+            },
+            {
+                title: 'The foundations of web typography',
+                time: '1h',
+                link: 'https://fonts.google.com/knowledge/using_type/the_foundations_of_web_typography',
+                linkText: 'The foundations of web typography'
+            },
+            {
+                title: 'Choosing the Right Font',
+                time: '1h',
+                link: 'https://careerfoundry.com/en/blog/ui-design/choosing-website-fonts/',
+                linkText: 'A Step-by-Step Guide to Choosing the Right Font for Your Website'
+            },
+            {
+                title: "Choose Brilliant Fonts for Any Website!",
+                time: '1h',
+                link: 'https://bowwe.com/en/blog/how-to-choose-best-fonts-website#',
+                linkText: "7 Easy & Proven Steps to Choose Brilliant Fonts for Any Website!"
+            }
+        ]
+    },
+    {
+        name: 'Fundamentals of Design',
+        day: 3,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: 'Design Fundamentals Basic',
+                time: '1h',
+                link: 'https://www.educative.io/answers/what-are-design-fundamentals',
+                linkText: 'What are Design Fundamentals?'
+            },
+            {
+                title: 'The basis of art, design',
+                time: '1h',
+                link: 'https://edu.gcfglobal.org/en/beginning-graphic-design/fundamentals-of-design/1/',
+                linkText: 'The basis of art, design, and more'
+            },
+            {
+                title: 'Design Principles',
+                time: '1h',
+                link: 'https://www.toptal.com/designers/visual/design-principles-hierarchy',
+                linkText: 'Design Principles: An Introduction to Visual Hierarchy'
+            },
+            {
+                title: 'Web design basics',
+                time: '1h',
+                link: 'https://99designs.com/blog/tips/web-design-basics-guide/',
+                linkText: 'Web design basics that will help you create a fantastic website'
+            },
+            {
+                title: "8 Web Design Principles",
+                time: '1h',
+                link: 'https://cxl.com/blog/universal-web-design-principles/',
+                linkText: "8 Web Design Principles and Laws that Work in 2023"
+            },
+            {
+                title: "Web design 101",
+                time: '1h',
+                link: 'assets/documents/Web design 101 - Webflow Ebook.pdf',
+                linkText: "Web design 101 - Webflow Ebook"
+            }
+        ]
+    },
+    {
+        name: 'Fundamentals of Design',
+        day: 4,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: "",
+                time: '1h',
+                link: '',
+                linkText: ""
+            },
+            {
+                title: "",
+                time: '1h',
+                link: '',
+                linkText: ""
+            },
+            {
+                title: "",
+                time: '1h',
+                link: '',
+                linkText: ""
+            },
+            {
+                title: "",
+                time: '1h',
+                link: '',
+                linkText: ""
+            },
+            {
+                title: "",
+                time: '1h',
+                link: '',
+                linkText: ""
+            },
+            {
+                title: "",
+                time: '1h',
+                link: '',
+                linkText: ""
+            }
+        ]
+    },
+    {
+        name: 'Photoshop',
+        day: 5,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: "Photoshop Get Started course",
+                time: '1h',
+                link: 'https://helpx.adobe.com/photoshop/tutorials.html',
+                linkText: "Photoshop Get Started course by Adobe"
+            },
+            {
+                title: "Beginner photo editing course",
+                time: '1h',
+                link: 'https://helpx.adobe.com/photoshop/tutorials.html',
+                linkText: "Beginner photo editing course by Adobe"
+            },
+            {
+                title: "Photoshop for Web Design Beginners",
+                time: '1h',
+                link: 'https://www.udemy.com/course/photoshop-for-web-design-beginners/learn/learn/',
+                linkText: "Udemy Course"
+            }
+        ]
+    },
+    {
+        name: 'Canva',
+        day: 6,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: "Canva for Beginners",
+                time: '1h',
+                link: 'https://www.canva.com/designschool/courses/',
+                linkText: "Canva for Beginners by Canva"
+            },
+            {
+                title: "Introduction to Canva ",
+                time: '1h',
+                link: 'https://alison.com/course/introduction-to-canva',
+                linkText: "Introduction to Canva, Alison Course"
+            }
+        ]
+    },
+    {
+        name: 'Figma',
+        day: 7,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: "Figma Design",
+                time: '2h',
+                link: 'https://www.figma.com/resource-library/',
+                linkText: "Dive into design topics and how to apply them with these resources from Figma"
+            },
+            {
+                title: "uxtoast figma course",
+                time: '2h',
+                link: 'https://www.uxtoast.com/ui-design/figma-get-started',
+                linkText: "uxtoast figma free course"
+            },
+            {
+                title: "Learn from figma youtube",
+                time: '2h',
+                link: 'https://youtube.com',
+                linkText: "Youtube"
+            }
+        ]
+    },
+    {
+        name: 'Website Layouts',
+        day: 8,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: "The fundamentals of website layout",
+                time: '1h',
+                link: 'https://99designs.com/blog/web-digital/website-layout-fundamentals/',
+                linkText: "The fundamentals of website layout"
+            },
+            {
+                title: "Layout Design Guide ",
+                time: '1h',
+                link: 'https://www.masterclass.com/articles/layout-design-guide',
+                linkText: "7 Tips for Designing a Layout"
+            },
+            {
+                title: "Youtube Channel",
+                time: '4h',
+                link: 'https://www.youtube.com/',
+                linkText: "Youtube"
+            },
+        ]
+    },
+    {
+        name: 'UI/UX',
+        day: 9,
+        badge: '',
+        time: '1d',
+        paragraph: 'In this lesson we will learn about colors. Color plays an important role in web design. After complete this lesson we will be able how to select and use colors to do a great website design.',
+        list: [
+            {
+                title: "Introduction to Design Thinking",
+                time: '1h',
+                link: 'https://www.mygreatlearning.com/academy/learn-for-free/courses/introduction-to-design-thinking',
+                linkText: "Learn the basics of design thinking"
+            },
+            {
+                title: "Fundamentals of User Interface Design",
+                time: '1h',
+                link: 'https://www.udemy.com/course/fundamentals-of-user-interface-design/',
+                linkText: "Fundamentals of User Interface Design -- Udemy"
+            },
+            {
+                title: "UI / UX for Beginners",
+                time: '1h',
+                link: 'https://www.mygreatlearning.com/academy/learn-for-free/courses/ui-ux',
+                linkText: "Learn UI UX Design from basics"
+            },
+            {
+                title: "User Experience (UX) Fundamentals",
+                time: '1h',
+                link: 'https://www.udemy.com/course/introtoux/',
+                linkText: "Learn the fundamentals of UX design and process."
+            }
+        ]
+    },
+];
+
+const webDesignLessons = document.getElementById('web_design__lessons');
+for(let i = 0; i < lessonsWebDesign.length; i++) {
+
+    const lessonList = document.createElement('div');
+    lessonList.setAttribute('class', 'lesson-list');
+    let lessonUl;
+
+    for(let j = 0; j < lessonsWebDesign[i].list.length; j++) {
+        const lessonListItem = `
+        <li>
+            ${lessonsWebDesign[i].list[j].title} 
+            <span class="text-muted">${lessonsWebDesign[i].list[j].time}</span>
+            <p>Resource: <a href="${lessonsWebDesign[i].list[j].link}">${lessonsWebDesign[i].list[j].linkText}</a></p>
+        </li>
+        `;
+        lessonUl += lessonListItem;
+    }
+
+    lessonList.innerHTML = lessonUl;
+    
+    const lesson = `
+        <div class="lesson-item">
+            <div class="row">
+                <div class="col-30">
+                    <div class="lesson-item--time">
+                        <span class="lesson-item--text">Day</span>
+                        <span class="lesson-item--number">${lessonsWebDesign[i].day}</span>
+                    </div>
+                </div>
+                
+                <div class="col-70 lesson-item--content">
+                    <!-- Lesson Item Header -->
+                    <div class="lesson-item--header mb-2">
+                        <div class="lesson-item--heading">
+                            <h3 class="mb-1">${lessonsWebDesign[i].name}</h3>
+                            <span class="badge-complete">Completed</span>
+                        </div>
+                        <div class="lesson-item--heading-timeline mb-2">
+                            <img src="assets/img/clock.svg" alt="Clock icon" width="22">
+                            <span>${lessonsWebDesign[i].time} to complete.</span>
+                        </div>
+                        <p>${lessonsWebDesign[i].paragraph}</p>
+                    </div>
+                    <!-- Lesson Item Content -->                       
+                    <div class="accordion" id="accordion__web-design">
+                        <h3 id="lesson_heading__photoshop">
+                            <button class="accordion-btn" type="button" aria-controls="lesson_content__photoshop">Lessons</button>
+                        </h3>
+                        <div id="lesson_content__photoshop" class="accordion-collapse" aria-labelledby="lesson_heading__photoshop">
+                            <div class="accordion-content">
+                                <h4 class="mb-1">Subjects that should We learn</h4>
+                                ${lessonList}
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/.col-70 -->
+            </div><!--/.row -->
+        </div><!--/.lesson-item -->
+    `;
+
+    webDesignLessons.innerHTML += lesson;
+}
+
+
+// Accordion
+const accordion = document.querySelectorAll('.accordion');
+accordion.forEach((item) => {
+    const accordionBtn = item.querySelector('.accordion-btn');
+    const accordionCollapse = item.querySelector('.accordion-collapse');
+
+    accordionBtn.addEventListener('click', () => {
+        accordionCollapse.classList.toggle('show');
+        setTimeout(() => {
+            accordionCollapse.classList.toggle('slide');
+        }, 100);
+        accordionBtn.classList.toggle('active');
+    })
+})
